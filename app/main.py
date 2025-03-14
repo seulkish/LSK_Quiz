8d32aa7a92eaace724e4512f70f3babc6b371151
root <chocomi77@gmail.com>
2025-03-14 22:36:48 +0900

    3/14 초기 셋팅

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
-# from fastapi.middleware.cors import CORSMiddleware
-# from app.api.api import api_router
-# from app.core.config import settings
+from fastapi.middleware.cors import CORSMiddleware
+from app.api.api import api_router
+from app.core.config import settings
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
@@ -12,17 +12,17 @@ app = FastAPI(
     redoc_url="/redoc",
 )
 
-# # CORS 설정
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=settings.BACKEND_CORS_ORIGINS,
-#     allow_credentials=True,
-#     allow_methods=["*"],
-#     allow_headers=["*"],
-# )
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # API 라우터 등록
-# app.include_router(api_router, prefix=settings.API_V1_STR)
+app.include_router(api_router, prefix=settings.API_V1_STR)
 
 @app.get("/")
 async def root():
